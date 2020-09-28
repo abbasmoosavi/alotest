3e766676f13d231dc176344e66d5ce6f2b07b2fc
Abbas Moosavi <moosaviabbas74@yahoo.com>
2020-09-28 12:23:56 +0330

    Feat: add assets as fonts and images

---
+++ src/assets/images/Images.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-unresolved */
+export default {
+  ic_about_us: require('./ic_about_us.png'),
+};
---
+++ src/assets/index.js
@@ -0,0 +1,2 @@
+export { default as Images } from './images/Images';
+export { default as DarkMap } from './jsons/DarkMap.json';
