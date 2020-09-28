6b88986e71727074f980c27efd20ae58e93417af
Abbas Moosavi <moosaviabbas74@yahoo.com>
2020-09-28 11:49:54 +0330

    Fix: set configs for prettire code

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  jsxSingleQuote: false,
+  printWidth: 100,
+  semi: true,
+  jsxBracketSameLine: true,
 };
